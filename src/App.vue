0366a6d50a378399f1e7f147539475db88a68e8b
Mohammadreza Khani <mohammadreza.khani.71@gmail.com>
2022-07-22 17:48:23 +0430

    fix: functionality bug feat: add prefix, suffix and splitter slots feat: add significant-figures attr feat: add keep-just-significant-figures attr fix: logaritmic attr renamed to exponential

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,58 @@
 <template>
-  <div dir="ltr" style="margin-left: 100px; font-size: 20px">
-    {{ state.value1 }} <input v-model.number="state.value1[0]" />
-    <VueSimpleRangeSlider
-      style="width: 1000px"
-      :min="0"
-      :max="1000000"
-      :exponential="true"
-      v-model="state.value1"
-      active-bar-color="red"
-      bar-color="blue"
-    />
-    {{ state.value2 }}
-    <VueSimpleRangeSlider
-      style="width: 100px"
-      :min="10"
-      :max="40"
-      v-model="state.value2"
-    />
+    <div dir="ltr" style="margin-left: 100px; font-size: 20px">
+        <VueSimpleRangeSlider
+            style="width: 500px"
+            :min="-10"
+            :max="1000000000"
+            :exponential="true"
+            v-model="state.value1"
+            active-bar-color="red"
+            :significant-figures="6"
+            bar-color="blue"
+        >
+            <template #suffix> ریال </template>
+            <template #prefix> </template>
+        </VueSimpleRangeSlider>
+        <VueSimpleRangeSlider style="width: 100px" :min="10" :max="40" v-model="state.value2" />
 
-    {{ state.value1 }}
-    <VueSimpleRangeSlider
-      style="width: 1000px; direction: rtl"
-      :min="0"
-      :max="1000000"
-      :exponential="true"
-      v-model="state.value1"
-      active-bar-color="#7e7e7e"
-      bar-color="#bebebe"
-      :significant-figures="4"
-    />
-    {{ state.value2 }}
-    <VueSimpleRangeSlider
-      style="width: 100px; direction: rtl"
-      :min="0"
-      :max="40"
-      v-model="state.value2"
-    />
-  </div>
+        <VueSimpleRangeSlider
+            style="width: 1000px; direction: rtl"
+            :min="0"
+            :max="1000000"
+            :exponential="true"
+            v-model="state.value3"
+            active-bar-color="#7e7e7e"
+            bar-color="#bebebe"
+            :significant-figures="3"
+        />
+        <VueSimpleRangeSlider
+            style="width: 100px; direction: rtl"
+            :min="-10"
+            :max="40"
+            v-model="state.value4"
+        />
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
-import VueSimpleRangeSlider from "./components/VueSimpleRangeSlider.vue";
+import { defineComponent, reactive } from 'vue';
+import VueSimpleRangeSlider from './components/simpleRangeSlider.vue';
 export default defineComponent({
-  components: { VueSimpleRangeSlider },
-  setup: () => {
-    const state = reactive<{ value1: [number, number]; value2: number }>({
-      value1: [0, 10],
-      value2: 3,
-    });
-    return { state };
-  },
+    components: { VueSimpleRangeSlider },
+    setup: () => {
+        const state = reactive<{
+            value1: [number, number];
+            value3: [number, number];
+            value2: number;
+            value4: number;
+        }>({
+            value1: [10, 1000],
+            value3: [10, 30],
+            value2: 3,
+            value4: 3
+        });
+        return { state };
+    }
 });
 </script>
 
